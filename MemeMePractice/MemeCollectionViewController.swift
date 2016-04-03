f8cfc7b7199297918267eca6db3cf68b3a5cc2d0
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-04-03 01:02:54 -0400

    added tabs and table/collection views

--- MemeMePractice/MemeCollectionViewController.swift
+++ MemeMePractice/MemeCollectionViewController.swift
@@ -2,17 +2,34 @@
 //  MemeCollectionViewController.swift
 //  MemeMePart2
 //
-//  Created by Gabriele on 3/28/16.
+//  Created by Gabriele on 4/3/16.
 //  Copyright © 2016 Ashley Donohoe. All rights reserved.
 //
 
 import UIKit
 
-class MemeCollectionViewController: UICollectionViewController {
-    var memes: [Meme]!
-    
+class MemeCollectionViewController: UIViewController {
+
     override func viewDidLoad() {
-        let applicationDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
-        memes = applicationDelegate.memes
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
--- MemeMePractice/MemeTableViewController.swift
+++ MemeMePractice/MemeTableViewController.swift
@@ -13,6 +13,8 @@ class MemeTableViewController: UITableViewController {
     var memes: [Meme]!
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         let applicationDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
         memes = applicationDelegate.memes
     }
